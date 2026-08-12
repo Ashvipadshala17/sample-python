aa242e4f51a77462fa42e609e4fe15c4e73ee373
Ashvi Padshala <152481701+Ashvipadshala17@users.noreply.github.com>
2026-08-12 13:42:27 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,127 @@
+# name = str(input("Enter your name: "))
+# print(name)
+
+# age = int(input("Enter your age: "))
+# print(age)
+
+# city = str(input("Enter your city: "))
+# print(city)
+
+# name = str(input("Enter your College name:"))
+# print(name)
+
+# color = str(input("Enter your favorite color: "))
+# print(color)
+
+# subject = str(input("Enter your favorite subject: "))
+# print(subject)
+
+# num = int(input("Enter your number:"))
+# print(num)
+# email = str(input("Enter your email address:"))
+# print(email)
+
+# name = str(input("Enter your Country name:"))
+# print(name)
+
+# msg = str(input("Welcome Message:"))
+# print(msg)
+
+# name = str(input("Enter your name: "))
+# print(name + " Welcome to the Spectrics Solutions")
+
+# age = int(input("Enter your age: "))
+# print("Your age is " +  str(age))
+
+# name = str(input("Enter your city:"))
+# state = str(input("Enter your state:"))
+# print("Your city is " + name + " and your state is " + state)
+
+# name = str(input("Enter your College name:"))
+# sem = int(input("Enter your SEM:"))
+# location = str(input("Enter the location:"))
+# print("your College name is " + name + " and Your SEM is " + str(sem) + " and your location is " + location)
+
+
+# num = int(input("Enter your number:"))
+# email = str(input("Enter your email address:"))
+# print("Your number is " + str(num) + " and your email address is " + email  )
+
+
+# name = str(input("Enter your Father name:"))
+# mother_name = str(input("Enter your Mother name:"))
+# print("Your Father name is " + name + " and your Mother name is " + mother_name)
+
+# name = str(input("Enter your Favourite Actor:"))
+# Actress_name = str(input("Enter your Favourite Actress:"))
+# print("Your favourite actore is  " + name + " and your favourite actress is " + Actress_name)
+
+# food = str(input("Enter your Favourite food:"))
+# drink = str(input("Enter your Favourite drink:"))
+# dessert = str(input("Enter your Favourite dessert:"))
+# print("Your favourite food is " + food + ", your favourite drink is " + drink + ", and your favourite dessert is " + dessert)
+
+# sport = str(input("Enter your Favourite sport:"))
+# player = str(input("Enter your Favourite player:"))
+# team = str(input("Enter your Favourite team:"))
+# print("Your favourite sport is " + sport + ", your favourite player is " + player + ", and your favourite team is " + team)
+
+# ig_user = str(input("Enter your Favourite ig_user:"))
+# link_user = str(input("Enter your Favourite link_user:"))
+# git_user = str(input("Enter your Favourite git_user:"))
+# print("Your favourite ig_user is " + ig_user + ", your favourite link_user is " + link_user + ", and your favourite git_user is " + git_user)
+
+# name = str(input("Enter your name:"))
+# email = str(input("Enter your email:"))
+# mobile_num = str(input("Enter your mobile number:"))
+# education = str(input("Enter your education:"))
+# skills = str(input("Enter your skills:"))
+# print("Your name is " + name + ", your email is " + email + ", and your mobile number is " + mobile_num)
+# print("Your education is " + education + ", and your skills are " + skills)
+
+
+# name = str(input("Enter Student name:"))
+# Roll_num = str(input("Enter Roll number:"))
+# College_name = str(input("Enter College name:"))
+# Course = str(input("Enter your course:"))
+# Semester = str(input("Enter your semester:"))
+# Division = str(input("Enter your division:"))
+# print( "Name: " +name )
+# print( "Roll Number: " + Roll_num ) 
+# print( "College Name: " + College_name)
+# print( "Course: " + Course ) 
+# print( "Semester: " + Semester )
+# print( "Division: " + Division)
+
+name = str(input("Enter Your First name:"))
+last_name = str(input("Enter Your Last name:"))
+Age = str(input("Enter Your Age:"))
+Gender = str(input("Enter Your Gender:"))
+mobile_num = str(input("Enter Mobile number:"))
+email = str(input("Enter Your Email:"))
+address = str(input("Enter Your Address:"))
+City = str(input("Enter Your City:"))
+State = str(input("Enter Your State:"))
+Country = str(input("Enter Your Country:"))
+College_name = str(input("Enter Your College name:"))
+Course = str(input("Enter Your Course:"))
+Semester = str(input("Enter Your Semester:"))
+Skills = str(input("Enter Your Skills:"))
+Hobby = str(input("Enter Your Hobby:"))
+
+print( "Name: " +name )
+print( "Last Name: " + last_name )
+print( "Age: " + Age )
+print( "Gender: " + Gender )
+print( "Mobile Number: " + mobile_num )
+print( "Email: " + email )
+print( "Address: " + address )
+print( "City: " + City )
+print( "State: " + State )
+print( "Country: " + Country )
+print( "College Name: " + College_name)
+print( "Course: " + Course ) 
+print( "Semester: " + Semester )
+print( "Skills: " + Skills )
+print( "Hobby: " + Hobby )
+
