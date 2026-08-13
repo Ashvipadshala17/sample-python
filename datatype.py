28dc2a6007bfff542030cac9f5b18bbe3d92f22c
Ashvi Padshala <152481701+Ashvipadshala17@users.noreply.github.com>
2026-08-13 13:52:04 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,94 @@
+# Lists
+# animal = ["cat", "dog", "rabbit", "lion", "tiger", "bear", "wolf", "fox", "deer"]
+# print(type(animal))
+# print(animal)
+
+# name = ['Ashvi','Moxa','Riya','Ishaan','Vivaan']
+# name.append('Yana')
+# print(name)
+
+# bird = ['parrot', 'sparrow', 'pigeon', 'crow', 'peacock']
+# bird.pop()
+# print(bird)
+
+# city = ['Ahm','Surat','Rajkot','Vadodara','Bhavnagar']
+# city.clear()
+# print(city)
+
+# things = ['pen', 'pencil', 'eraser', 'sharpener', 'notebook']
+# things.copy()
+# print(things)
+
+# ele = ['mobile', 'laptop', 'tablet', 'desktop', 'smartwatch']
+# ele.sort()
+# print(ele)
+
+# clothes = ['shirt', 'pant', 't-shirt', 'jacket', 'sweater']
+# clothes.reverse()
+# print(clothes)
+
+# furniture = ['sofa', 'table', 'chair', 'bed', 'cabinet']
+# furniture.remove('table')
+# print(furniture)
+
+# fruites = ['apple', 'banana', 'orange', 'grape', 'kiwi']
+# fruites.insert(2, 'mango')
+# print(fruites)
+
+# flowers = ['rose', 'lily', 'tulip', 'daisy', 'sunflower']
+# print(flowers.index('tulip'))
+
+# vegetables = ['carrot', 'broccoli', 'spinach', 'cauliflower', 'pepper']
+# print(len(vegetables))
+
+# App = ['Instagram', 'Facebook', 'Twitter', 'Snapchat', 'WhatsApp', 'Telegram', 'LinkedIn', 'Pinterest']
+# print(App[3])
+# print(App[1:3])
+# print(App[0:7:2])
+
+
+# Tuples 
+# animals = ('cat', 'dog', 'rabbit', 'lion', 'tiger', 'bear', 'wolf', 'fox', 'deer')
+# print(type(animals))
+# birds = ('parrot', 'sparrow', 'pigeon', 'crow', 'peacock')
+# birds = animals.count('cat')
+# birds = animals.index('rabbit')
+# print(birds)
+# print(animals)
+
+
+
+
+
+colour = ['red', 'blue', 'green', 'yellow','red', 'orange', 'purple']
+# colour.append('pink')
+# colour.remove('yellow')
+# print(len(colour))
+# print("First element:", colour[0])
+# print("Last element:", colour[-1])
+# colour[2] = 'white'
+# colour.sort()
+# bird = ['parrot', 'sparrow', 'pigeon', 'crow', 'peacock']
+# bird = colour.count('red')
+# print(bird)
+# colour.reverse()
+List = tuple(colour)
+print(List)
+
+# numbers = [25, 10, 45, 30, 15]
+# maximum = max(numbers)
+# print(maximum)
+
+
+
+
+# fruits = ('apple', 'banana', 'orange', 'grape', 'kiwi')
+# print("Second element:", fruits[1])
+# print("fourth element:", fruits[3])
+# print(len(fruits))
+# colours = ['red', 'blue', 'green', 'yellow', 'orange']
+# colurs = fruits.index('orange')
+# print(colurs)
+# print("Last element:", fruits[-1])
+# List = list(fruits)
+# print(List)
